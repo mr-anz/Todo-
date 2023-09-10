051d96b2b71517bf1fc2ae93f7ceb0257278255c
Aneez <mr.aneez04@gmail.com>
2023-09-10 12:18:19 +0530

    second

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     isWallectConnected()
   },[])
   return (
-    <div className="h-full glass bg-[#1e1e21]">
+    <div className="h-screen glass bg-[#1e1e21]">
       <Navbar/>
       <div className="flex items-center justify-center">
         <div className="  w-full p-5 sm:px-0 mt-20 sm:w-5/6 lg:w-2/5 ">
